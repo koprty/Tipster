69797fefe207054b1e63aec34916f15642af5c6c
koprty <koprty@gmail.com>
2015-12-12 15:13:01 -0500

    initialized Tipster swift project

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by Lise Ho on 12/12/15.
+//  Copyright © 2015 lise_ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var amountSegCtrl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var inputLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
